40e8a280f75da8f9a5e388003a1ac6cd7c6e4ea9
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-07-08 21:31:46 -0400

    Add validations for attributes of item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+  validates :merchant_id, presence: true
   belongs_to :merchant
 end
