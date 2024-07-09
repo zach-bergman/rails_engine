7140917d760431ddd8a102f23f8a150e21cd7c9e
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-07-08 21:34:40 -0400

    Add test for validations of item attributes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,9 @@ describe Item, type: :model do
   describe "relationships" do
     it { should belong_to :merchant }
   end
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :unit_price }
+  it { should validate_presence_of :merchant_id }
 end
