ed590cf0a346194b9a3cc854c03a3a5941a89dc7
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-07-08 21:31:10 -0400

    Add method create with an specific error method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,23 @@
 class Api::V1::ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :missing_attributes
   def index
     render json: ItemSerializer.new(Item.all)
   end
 
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    render json: ItemSerializer.new(Item.create!(item_params)), status: 201
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+  def missing_attributes(execution)
+    render json: ErrorSerializer.new(ErrorMessage.new(execution.message, 400)).error, status: 400
   end
 end
